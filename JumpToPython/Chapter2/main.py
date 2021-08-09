35c7bdaa4620ed4606135c08ca7607eb174dd589
leejeongeun98 <je981@naver.com>
2021-08-09 16:33:09 +0900

    basic

---
+++ JumpToPython/Chapter2/main.py
@@ -0,0 +1,71 @@
+# # 정수형
+# a = 123
+# a = -178
+# a = 0
+#
+# # 실수형
+# a = 1.2
+# a = -3.35
+# a = 4.34E10
+# a = 4.24e-10
+# # 8진수
+# a = 0o1777
+# # 16진수
+# a = 0x8ff
+# b = 0xABC
+#
+# # 사칙연산
+# a = 3
+# b = 4
+# print(a + b)
+# print(a * b)
+# print(a / b)
+#
+# # x의 y제곱을 나타내는 ** 연산자
+# a = 3
+# b = 4
+# print(a ** b)
+#
+# # 나눗셈 후 나머지를 반환하는 % 연산자
+# print(7 % 3)
+# print(3 % 7)
+#
+# # 나눗셈 후 몫을 반환하는 // 연산자
+# print(7 / 4)
+# print(7 // 4)
+
+# # 문자열
+# print("hello world")
+# print('python is fun')
+# print("""life is too short, you need python""")
+# print('''life is too short, you need python''')
+#
+# food = "python's favorite food is perl"
+# say = '"python is very easy." he says.'
+# food = 'python\'s favorite food is perl'
+# say = "\"python is very easy.\"he says."
+
+multiline = "life is too short\n you need python"
+multiline = '''
+life is too short
+you need python 
+'''
+print(multiline)
+
+head = "python"
+tail = " is fun!"
+head + tail
+
+a = "python"
+a * 2
+
+print("=" * 50)
+print("my program")
+print("=" * 50)
+
+a = 'life is to short'
+len(a)
+
+a = "life is too short, you need python"
+a[3]
+
